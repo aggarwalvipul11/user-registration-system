87766a4bc1004999de5dd673f054451bdd714d3a
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-06 07:11:16 +0530

    Completed Usercase01

---
+++ userRegistration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+function condCheck() {
+	if [[ $1 =~ $2 ]]
+	then
+		echo "valid"
+	else
+		echo "invalid"
+	fi
+}
+
+read -p "Enter your first name " fName
+
+namePattern="^[A-Z][a-z]{2,}"
+condCheck $fname $namePattern
+
