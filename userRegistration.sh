2732dea8cd2987daf4ed45cad4d4f70290bc6078
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-06 08:10:47 +0530

    Completed Usercase07

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,7 +3,7 @@
 namePattern="^[A-Z][a-z]{3,}"
 emailPattern="^[A-Za-z0-9]*[\-\+\.0-9]*[a-zA-z0-9]+[@][a-z0-9]+[.]+[a-z]{2,3}$"
 phoneNumberPattern="^[0-9]{2}(\s)[0-9]{10}$"
-passwordPattern="^[a-zA-Z]{8,}*[A-Z]{1,}"
+passwordPattern="^[a-zA-Z]{8,}*[A-Z]{1,}*[0-9]{1,}"
 
 function condCheck() {
 	if [[ $1 =~ $2 ]]
@@ -26,5 +26,5 @@ condCheck $validEmail $emailPattern
 read -p "Enter your phone number: " validPhoneNumber
 condCheck $validPhoneNumber $phoneNumberPattern
 
-read -p "Enter your password(Must have atleast 8 characters, 1 Uppercase letter): " vaildPassword
+read -p "Enter your password(Must have atleast 8 characters, 1 Uppercase letter, 1 Number): " vaildPassword
 condCheck $validPassword $passwordPattern
