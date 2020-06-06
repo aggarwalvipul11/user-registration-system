f65a822dd58ce36fe10c63a840114c2b51de05ad
aggarwalvipul11 <65864933+aggarwalvipul11@users.noreply.github.com>
2020-06-06 16:41:29 +0530

    Update userRegistration.sh

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 namePattern="^[A-Z][a-z]{3,}"
-emailPattern="^[A-Za-z0-9]*[\-\+\.0-9]*[a-zA-z0-9]+[@][a-z0-9]+[.]+[a-z]{2,3}$"
+emailPattern="^[A-Za-z0-9]*[\-\+\.0-9]*[a-zA-z0-9]+[@][a-z0-9]+[.]+[a-z]*[.]*[a-z]{2,3}$"
 phoneNumberPattern="^[0-9]{2}(\s)[0-9]{10}$"
 
 function condCheck() {
@@ -14,10 +14,10 @@ function condCheck() {
 }
 
 read -p "Enter your first name " fName
-condCheck $fname $namePattern
+condCheck $fName $namePattern
 
 read -p "Enter your last name " lName
-condCheck $lname $namePattern
+condCheck $lMame $namePattern
 
 read -p "Enter your email id " validEmail
 condCheck $validEmail $emailPattern
